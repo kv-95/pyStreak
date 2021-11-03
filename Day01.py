3618f0906d98b0a72ed8a441629792e6eb921676
christymthmas <85362790+christymthmas@users.noreply.github.com>
2021-11-03 20:00:40 +0530

    init

--- Day01.py
+++ Day01.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+#Maximum of three numbers
+
+def findMax(num1,num2,num3):
+    if num1>num2 & num1>num3:
+        max = num1
+    elif num2>num1 & num2>num3:
+        max = num2
+    else:
+        max = num3
+    return max
+ 
+if __name__ == "__main__":
+    num1 = int(input("Enter the first number : "))
+    num2 = int(input("Enter the second number : "))
+    num3 = int(input("Enter the third number : "))
+    maximum = findMax(num1, num2, num3)
+    print("The highest among the three numbers is", maximum)
