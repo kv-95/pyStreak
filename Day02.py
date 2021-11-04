9b7e65433257832d84fba65958adc9ba8dbe30f2
christymthmas <85362790+christymthmas@users.noreply.github.com>
2021-11-04 15:00:45 +0530

    final

--- Day02.py
+++ Day02.py
@@ -1,1 +1,12 @@
-#
+#Python Program for factorial of a number
+
+def fact(num):
+    if num==0:
+        return 1
+    else:
+        return num*fact(num-1)
+
+if __name__ == "__main__":
+    num = int(input("Enter the number : "))
+    fact = fact(num)
+    print("The factorial of {} is {}".format(num,fact))
